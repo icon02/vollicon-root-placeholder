fc34dbb83b43e7b24a4081b451860691639ed90e
Nico <nico.layr@gmx.at>
2021-08-10 14:46:47 +0200

    added express server

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import path from "path";
+const app = express();
+const port = process.env.PORT || 3100;
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+})
