e36717ea3db4849bb1aa27240bc96976c5cad5e0
Nico <nico.layr@gmx.at>
2021-08-10 14:38:57 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import MichiSrc from "./assets/god_natt.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{padding: 0, margin: 0, width: "100vw", height: "100vh", backgroundImage: `url(${MichiSrc})`, backgroundRepeat: "repeat", backgroundSize: "contain", backgroundPosition: "center"}}>
     </div>
   );
 }
